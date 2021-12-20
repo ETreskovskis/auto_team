a6bba9d9f5f1bebadc10eb20af4982db91e4f986
erikas.treskovskis <erikas.treskovskis@auriga.com>
2021-12-20 10:39:38 +0200

    - changes > main_runner.py created, parameters are shorter, README.md modified

--- auto_join_teams_meeting.py
+++ auto_join_teams_meeting.py
@@ -690,28 +690,4 @@ class TeamsRunner:
         return True, meetings_results
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Teams AUTO-JOIN. For additional parameter info use --help")
-    parser.add_argument("--mic_state", type=str, required=True, default="off",
-                        help="Provide flag for microphone: 'on' or 'off'. Note: this set up for all upcoming meetings",
-                        )
-    parser.add_argument("--camera_state", type=str, required=True, default="off",
-                        help="Provide flag for camera: 'on' or 'off'. Note: this set up for all upcoming meetings",
-                        )
-    parser.add_argument("--start_before", type=int, required=False,
-                        help="Provide time (seconds) to join before actual meeting has started",
-                        default=3 * 60)
 
-    arguments = parser.parse_args()
-
-    outlook_class = OutlookApi(time_before=arguments.start_before)
-    planned_meetings = outlook_class.available_meetings()
-    wrapp_iui_auto = partial(IUIAutomation, camera=arguments.camera_state, mic=arguments.mic_state)
-    enum_class = EnumActiveWindows()
-    mouse_event = MouseEvents()
-    run_meetings_bool, run_meetings_list = TeamsRunner.run_meetings(planned_meetings, enum=enum_class,
-                                                                    iui_auto=wrapp_iui_auto,
-                                                                    outlook=outlook_class, mouse=mouse_event)
-    if not run_meetings_bool:
-        sys.exit("There are no meetings to start. Quiting.")
-    sys.exit(f"Quiting threads. Finished meetings: {*run_meetings_list,}")
